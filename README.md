5a1c4913991c52d2d3146bbc9bd3c0bb3574283b
George Burslem <George@bursomac.home>
2019-10-13 19:40:12 +0100

    Final commit. The solution is now more general and would work with future items/discounts added.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Kata checkout system for a coding challenge.
+
+To get a total, create ScannedItem's in the Main method and scan them with "checkout.Scan(ScannedItem)".
--- lib/checkout_kata.cs
+++ lib/checkout_kata.cs
@@ -6,9 +6,14 @@ namespace Kata
 {
     public class Checkout
     {
-        List<Item> scannedItems = new List<Item>();
+        List<ScannedItem> scannedItems = new List<ScannedItem>();
+        List<BasketItem> basket = new List<BasketItem>();
 
-        public float GetTotal()
+        /*
+        *   Returns the discounted total of the scanned items. Also prints the non discounted 
+        *   total and the total amount saved from discounts. 
+         */
+        public float GetDiscountedTotal()
         {
             float total = 0;
 
@@ -17,7 +22,7 @@ namespace Kata
             {
                 total += scannedItems[i].price;
             }
-            Console.WriteLine("Total: " + total);
+            Console.WriteLine("Non Discounted Total: " + total);
 
             float discountAmount = GetDiscountAmount();
             Console.WriteLine("Amount saved: " + discountAmount + "\n");
@@ -25,73 +30,140 @@ namespace Kata
             return total - discountAmount;
         }
 
+        /*
+        *   Returns the total amount saved from discounts. First needs to create a basket
+        *   from scanned items, and then calculates discounts based on the quantities of 
+        *   items. 
+         */
         private float GetDiscountAmount()
         {
-            float discountAmount = 0;
-            int totalA99 = 0;
-            int totalB15 = 0;
 
+            CreateBasket();
+            PrintBasket();
+
+            float discount = CalculateDiscount();
+            return discount;
+        }
+
+        /*
+        * Uses the scanned items to create a basket with item quantities. 
+         */
+        private void CreateBasket()
+        {
             for (int i = 0; i < scannedItems.Count; i++)
             {
-                if (scannedItems[i].name == "A99")
+                if (basket.Count != 0)
                 {
-                    totalA99 += 1;
+                    AddItemToBasket(i);
                 }
-                if (scannedItems[i].name == "B15")
+                else
                 {
-                    totalB15 += 1;
+                    BasketItem basketItem = new BasketItem(scannedItems[i].getName(),
+                                                    scannedItems[i].getPrice(),
+                                                    scannedItems[i].getDiscountAmount(),
+                                                    scannedItems[i].getDiscountPrice());
+                    basket.Add(basketItem);
                 }
             }
-
-            Console.WriteLine("Total A99: " + totalA99 + " Total B15: " + totalB15);
-
-            discountAmount = CalculateDiscounts(totalA99, totalB15);
-            return discountAmount;
         }
 
-        private float CalculateDiscounts(int totalA99, int totalB15)
+        /*
+        * Adds a scanned item to the basket. Will check if an item of the same type has been added,
+        * and if so, will just increase the quantity of that item. 
+         */
+        private void AddItemToBasket(int scannedItemIndex)
         {
-            int numberOfA99Deals = totalA99 / 3;
-            float a99DiscountTotal = numberOfA99Deals * (1.5f - 1.3f);
-
-            int numberOfB15Deals = totalB15 / 2;
-            float b15DiscountTotal = numberOfB15Deals * (0.6f - 0.45f);
-
-            return a99DiscountTotal + b15DiscountTotal;
+            bool found = false;
+            for (int j = 0; j < basket.Count; j++)
+            {
+                if (basket[j].name == scannedItems[scannedItemIndex].name)
+                {
+                    basket[j].addOne();
+                    found = true;
+                    break;
+                }
+            }
+            if (found == false)
+            {
+                BasketItem basketItem = new BasketItem(scannedItems[scannedItemIndex].getName(),
+                                                    scannedItems[scannedItemIndex].getPrice(),
+                                                    scannedItems[scannedItemIndex].getDiscountAmount(),
+                                                    scannedItems[scannedItemIndex].getDiscountPrice());
+                basket.Add(basketItem);
+            }
         }
 
-        public void Scan(Item item)
+        /*
+        *   Prints the content of a basket.
+         */
+        public void PrintBasket()
+        {
+            for (int i = 0; i < basket.Count; i++)
+            {
+                Console.WriteLine("Basket Name: " + basket[i].name);
+                Console.WriteLine("Basket Quantity: " + basket[i].quantity);
+            }
+        }
+
+        /*
+        *   Returns the total amount saved with the discounts on the items in the basket.
+         */
+        private float CalculateDiscount()
+        {
+            float totalDiscount = 0;
+            for (int i = 0; i < basket.Count; i++)
+            {
+                int numberOfDeals = basket[i].quantity / basket[i].discountAmount;
+                float savingPerDeal = (basket[i].price * basket[i].discountAmount) - basket[i].discountPrice;
+                totalDiscount += numberOfDeals * savingPerDeal;
+            }
+            return totalDiscount;
+        }
+
+        /*
+        *   Adds item to scanned items list.
+         */
+        public void Scan(ScannedItem item)
         {
             // Add item to scanned items
             scannedItems.Add(item);
         }
 
+        /*
+        *   Main method where some example scanned items are added and the discounted
+        *   total is calculated and displayed.
+         */
         static void Main(string[] args)
         {
             Checkout checkout = new Checkout();
 
-            Item item1 = new Item("A99", 0.5f, 3f, 1.30f);
-            Item item2 = new Item("B15", 0.3f, 2f, 0.45f);
-            Item item3 = new Item("A99", 0.5f, 3f, 1.30f);
-            Item item4 = new Item("A99", 0.5f, 3f, 1.30f);
+            ScannedItem item1 = new ScannedItem("A99", 0.5f, 3, 1.30f);
+            ScannedItem item2 = new ScannedItem("B15", 0.3f, 2, 0.45f);
+            ScannedItem item3 = new ScannedItem("A99", 0.5f, 3, 1.30f);
+            ScannedItem item4 = new ScannedItem("A99", 0.5f, 3, 1.30f);
+            ScannedItem item5 = new ScannedItem("B15", 0.3f, 2, 0.45f);
 
             checkout.Scan(item1);
             checkout.Scan(item2);
             checkout.Scan(item3);
             checkout.Scan(item4);
+            checkout.Scan(item5);
 
-            Console.WriteLine("Discounted total: " + checkout.GetTotal() + "\n");
+            Console.WriteLine("Discounted total: " + checkout.GetDiscountedTotal() + "\n");
         }
     }
 
-    public class Item
+    /*
+    *   Class for scanned items. 
+     */
+    public class ScannedItem
     {
         public string name;
         public float price;
-        public float discountAmount;
+        public int discountAmount;
         public float discountPrice;
 
-        public Item(string name, float price, float discountAmount, float discountPrice)
+        public ScannedItem(string name, float price, int discountAmount, float discountPrice)
         {
             this.name = name;
             this.price = price;
@@ -99,6 +171,52 @@ namespace Kata
             this.discountPrice = discountPrice;
         }
 
+        public string getName()
+        {
+            return this.name;
+        }
+
+        public float getPrice()
+        {
+            return this.price;
+        }
+
+        public int getDiscountAmount()
+        {
+            return this.discountAmount;
+        }
+
+        public float getDiscountPrice()
+        {
+            return this.discountPrice;
+        }
+
+    }
+
+    /*
+    * Class for basket items. These items have quantites. 
+     */
+    public class BasketItem
+    {
+        public string name;
+        public float price;
+        public int quantity;
+        public int discountAmount;
+        public float discountPrice;
+
+        public BasketItem(string name, float price, int discountAmount, float discountPrice)
+        {
+            this.name = name;
+            this.price = price;
+            this.discountAmount = discountAmount;
+            this.discountPrice = discountPrice;
+            this.quantity = 1;
+        }
+
+        public void addOne()
+        {
+            this.quantity += 1;
+        }
     }
 
 }
